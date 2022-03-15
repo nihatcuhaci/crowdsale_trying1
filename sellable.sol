4d1c665427a7b8e2a5e5be207552fc1eb0c83937
nihatcuhaci <95506698+nihatcuhaci@users.noreply.github.com>
2022-03-15 21:07:23 +0300

    Update sellable.sol

--- sellable.sol
+++ sellable.sol
@@ -3,23 +3,50 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 contract Sellable {
-    uint price =1;
     IERC20 public token;
-    address payable skimmer;
-    constructor(address _token, uint _price) public {
-        token= IERC20(_token);
-        price= _price;
-        skimmer= payable(msg.sender);
+    uint price;
+    uint256 maxAmountPerAddress;
+    uint256 public constant one = 10**18;
+    mapping (address => uint) addressTokenCount;
+    uint256 tokensUnlockTime;
+    bool Paused = false;
+    address owner;
+
+    event Bought(uint256 amount);
+
+    constructor(address _token,uint _price, uint256 _maxAmountPerAddress, uint256 _tokensUnlockTime) {
+        token = IERC20(_token);
+        price = _price;
+        maxAmountPerAddress = _maxAmountPerAddress;
+        tokensUnlockTime = _tokensUnlockTime;
+        owner = msg.sender;
     }
 
     function buy(uint amount) external payable {
-        require (msg.value > amount*price, "not enough ETH");
-        token.transfer(msg.sender, amount);
+       require(Paused == false);
+        require(tokensUnlockTime < block.timestamp, " unlocked!");
+        require(addressTokenCount[msg.sender] < maxAmountPerAddress, "there is no enough token");
+        require(msg.value > amount*price*one,"not enough eth");
+        token.transfer(msg.sender,amount);
+        addressTokenCount[msg.sender] += amount;
+        emit Bought(amount);
     }
 
-    function skim () external {
-        skimmer.transfer(payable(address(this)).balance);
-
+   function pauseSale() public {
+        require(msg.sender == owner);
+        Paused = true;
     }
 
+    function continueSale() public {
+        require(msg.sender == owner);
+        Paused = false;
+    }
+
+    function viewPrice() public view returns(uint) {
+        return price;
+    }
+
+    function viewUnlockTime() public view returns(uint256) {
+        return tokensUnlockTime;
+    }
 }
